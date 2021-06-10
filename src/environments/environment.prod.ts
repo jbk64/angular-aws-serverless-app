cb163cc776e7a53675b7a5ec7d45ff610312f08d
Jack <j.barker@infomir.com>
2021-06-10 16:53:33 +0200

    fix prod env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   awsUserPoolId: 'eu-central-1_38CR8ssVT',
-  awsUserPoolClientId: '5jifajl0npgedvb04plh28i3gu'
+  awsUserPoolClientId: '5jifajl0npgedvb04plh28i3gu',
+  awsWebsocketApiUrl: 'wss://fr058vhcw5.execute-api.eu-central-1.amazonaws.com/dev/'
 };
